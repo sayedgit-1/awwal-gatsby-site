28daeaf1cf2b4ba876f8f8c9b6bc3e052323a5f8
Sayed Ariful Hoque <sayedarifulhoque@Sayeds-MacBook-Air.local>
2023-07-11 10:59:51 +0530

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,13 +1,21 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>My blog post contents will go here (eventually).</p>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Posted : {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit : {""}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   );
@@ -18,7 +26,15 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D,YYYY")
+        date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
